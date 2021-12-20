194f5d209fcb6a87e1c0e7fb281ea442988d39e0
Fabiano Gois <fabiiano.gois@gmail.com>
2021-12-19 22:21:21 -0300

    Finalizando exercícios e exemplos com loops e arrays

--- loops-e-arrays/src/br.com.dio.exercicios/loops/Ex7_ExerForWhile.java
+++ loops-e-arrays/src/br.com.dio.exercicios/loops/Ex7_ExerForWhile.java
@@ -12,19 +12,26 @@ public class Ex7_ExerForWhile {
 
     public static void main(String[] args) {
 
-        int numeros = 0;
         boolean verdade = true;
         int count = 0;
 
+        System.out.println("Números de 3 a 11, deixando o 7 de fora");
         for (int i = 3;i <= 11; i = i + 2) {
-            System.out.println(i);
+            if (i == 7) continue;
+            System.out.print(i+" ");
         }
 
+        System.out.println("\nNúmeros de 1 a 14, mostrando apenas os múltiplos de 2"+
+                             " e encerrando ao chegar no número 12, sem mostrar ele.");
+        for (int i = 1;i <= 14; i++) {
+                if (i == 12) break;
+                if (i % 2 == 0) System.out.print(i + " ");
+        }
+        System.out.println("\n");
         while (verdade && count < 5){
             System.out.println("Texto aleatório!");
             count++;
         }
 
     }
-
 }
